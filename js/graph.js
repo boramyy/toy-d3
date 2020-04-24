db986f87f52e4bdbec9a698581f508090afe499a
rodykim <rody.kim@kakaocorp.com>
2020-04-24 09:26:41 +0900

    눈금 갯수 정리 필요

--- js/graph.js
+++ js/graph.js
@@ -4,75 +4,88 @@
     {
       "month": 12,
       "charge": 28,
-      "amount": 3000
+      "amount": 60000
     },
     {
       "month": 1,
       "charge": 50,
-      "amount": 2500
+      "amount": 55000
     },
     {
       "month": 2,
       "charge": 10,
-      "amount": 1500
+      "amount": 95000
     },
     {
       "month": 3,
       "charge": 80,
-      "amount": 3000
+      "amount": 82300
     },
     {
       "month": 4,
       "charge": 60,
-      "amount": 1000
+      "amount": 100000
     },
     {
       "month": 5,
       "charge": 30,
-      "amount": 3000
+      "amount": 72000
     },
     {
       "month": 6,
       "charge": 20,
-      "amount": 2500
+      "amount": 93000
     },
     {
       "month": 7,
       "charge": 10,
-      "amount": 2000
+      "amount": 42000
     },
     {
       "month": 8,
       "charge": 10,
-      "amount": 3000
+      "amount": 50000
     },
     {
       "month": 9,
       "charge": 65,
-      "amount": 3000
+      "amount": 56000
     },
     {
       "month": 10,
       "charge": 17,
-      "amount": 3000
+      "amount": 90000
     },
     {
       "month": 11,
       "charge": 30,
-      "amount": 3000
+      "amount": 50000
     },
     {
       "month": 12,
       "charge": 28,
-      "amount": 3000
+      "amount": 73000
     }
   ]
+
+  function maxNum(arr, key) {
+    var num = arr[0][key] || -1
+    for (var i = 0; i < arr.length; i ++) {
+      var currentNum = arr[i][key]
+      if (num < currentNum) {
+        num = currentNum
+      }
+    }
+    return num
+  }
   
   var MAX_HEIGHT = 169;
   var BAR_WIDTH = 4
   var BAR_MARGIN_RIGHT = 15
-  var MAX_CHARGE = 80
-  var MAX_AMOUNT = 7000
+  var MAX_CHARGE = maxNum(data, 'charge')
+  var MAX_AMOUNT = maxNum(data, 'amount')
+  var X_START_POINT = 53
+  var Y_START_POINT = 5
   
   window.onload = function() {
 
@@ -84,8 +97,8 @@
       .data(data)
       .join('rect')
       .classed('bar', true)
-      .attr("x", function (d, i) { return i * (BAR_WIDTH + BAR_MARGIN_RIGHT)})
-      .attr("y", function (d) { return MAX_HEIGHT - barScale(d.charge)})
+      .attr("x", function (d, i) { return i * (BAR_WIDTH + BAR_MARGIN_RIGHT) + X_START_POINT})
+      .attr("y", function (d) { return MAX_HEIGHT - barScale(d.charge) + Y_START_POINT})
       .attr('fill', '#FFCD00')
       .attr('rx', '2px')
       .attr('ry', '2px')
@@ -94,14 +107,14 @@
 
 
     // 꺾은선
-    var data2 = [[0, 100], [15, 3000], [30, 1000], [45, 1350], [60, 7000], [75, 2500], [90, 3000], [105, 1000], [120, 1350], [135, 5000], [150, 5000], [165, 1000], [180, 1350]];
+    // var data2 = [[0, 100], [15, 3000], [30, 1000], [45, 1350], [60, 7000], [75, 2500], [90, 3000], [105, 1000], [120, 1350], [135, 5000], [150, 5000], [165, 1000], [180, 1350]];
     var lineWidth = BAR_WIDTH + BAR_MARGIN_RIGHT
     var shapeScale = d3.scaleLinear().domain([MAX_AMOUNT, 0]).range([0, MAX_HEIGHT])
     var lineGenerator = d3.line()
-      .x(function (d, i) { return i * lineWidth + 2 })
-      .y(function (d) { return shapeScale(d[1]) })
+      .x(function (d, i) { return i * lineWidth + 2 + X_START_POINT })
+      .y(function (d) { return shapeScale(d.amount) + Y_START_POINT })
     
-    var pathString = lineGenerator(data2)
+    var pathString = lineGenerator(data)
 
     d3.select('#hoho2')
       .append('g')
@@ -124,17 +137,24 @@
 
     d3.select('#hoho2')
       .append('g')
-      .attr('transform', 'translate(50, 0)')
-      // .append('path')
-      .call(d3.axisLeft(shapeScale))
+      .attr('transform', 'translate(53, 0)')
+      .call(d3.axisLeft(shapeScale.range([0 + Y_START_POINT, MAX_HEIGHT + Y_START_POINT])).ticks(5))
+      .style('font-size', '12px')
+      .style('color', '#999')
+      .call(g => g.select(".domain").remove())
+      .call(g => g.selectAll("line").remove())
 
     d3.select('#hoho2')
       .append('g')
-      .attr('transform', 'translate(100, 0)')
-      // .append('path')
-      .call(d3.axisRight(barScale.domain([MAX_CHARGE,0])))
+      .attr('transform', 'translate(288, 0)')
+      .call(d3.axisRight(barScale.domain([MAX_CHARGE, 0]).range([0 + Y_START_POINT, MAX_HEIGHT + Y_START_POINT])).ticks(5))
+      .style('font-size', '12px')
+      .style('color', '#999')
+      .call(g => g.select(".domain").remove())
+      .call(g => g.selectAll("line").remove())
   }
   
-
+  // 45+8 : 227 : 38+8
+  //
   
 })()
